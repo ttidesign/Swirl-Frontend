c59d271ad326ecc5739b41bc56ef547aa1bed8c7
ttdesign <tkthedesigner@gmail.com>
2020-06-06 23:45:33 -0700

    inttall bootstrap, create navbar

--- swirl-app/src/App.js
+++ swirl-app/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar'
 
 function App() {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+		getDatas();
+		//eslint-disable-next-line
+  }, []);
+  function getDatas() {
+		const url = `http://localhost:8000/`;
+		//fetch data
+		fetch(url)
+			.then((response) => response.json())
+			.then((response) => {
+				console.log(response)
+				//set datas value and assign to datas array
+				setDatas(response);
+				// display last search item on page
+			})
+			.catch(console.error);
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div>
+			<NavBar />
+			<h2>hello</h2>
+		</div>
+	);
 }
 
 export default App;
